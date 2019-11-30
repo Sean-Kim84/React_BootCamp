c185e9101b9f472a408593cd73cdcdaf45bf2708
Sean-Kim84 <sean.kim843@gmail.com>
2019-11-30 11:58:14 +0900

    Be use the setState as a Updater

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -29,30 +29,48 @@ var Counter = function (_React$Component) {
     _createClass(Counter, [{
         key: "handleAddOne",
         value: function handleAddOne() {
-            this.setState(function (prevState) {
-                return {
-                    count: prevState.count + 1
-                };
+            this.setState({
+                count: this.state.count + 1
             });
+            // this.setState((prevState) => {
+            //     return {
+            //         count: prevState.count + 1
+            //     };
+            // });
             // this.state.count = this.state.count +1;
             console.log(this.state);
         }
     }, {
         key: "handleMinusOne",
         value: function handleMinusOne() {
-            this.setState(function (prevState) {
-                return {
-                    count: prevState.count - 1
-                };
+            // this.setState((prevState) => {
+            //     return {
+            //         count: prevState.count-1
+            //     }
+            // }) 
+            this.setState({ //prevState에 직접접근
+                count: this.state.count - 1
             });
             console.log(this.state);
         }
     }, {
         key: "handleReset",
         value: function handleReset() {
+            // this.setState(() => {
+            //     return {
+            //         count: 0 
+            //     }
+            // })
             this.setState(function () {
+                // 이 방식의 setState는 component render를 두번하게된다(아래함수도 실행)
                 return {
                     count: 0
+                };
+            });
+
+            this.setState(function (prevState) {
+                return {
+                    count: prevState.count + 1
                 };
             });
         }
--- indecision-app/src/playground/counter2.js
+++ indecision-app/src/playground/counter2.js
@@ -11,30 +11,48 @@ class Counter extends React.Component {
     }
 
     handleAddOne() {
-        this.setState((prevState) => {
-            return {
-                count: prevState.count + 1
-            };
-        });
+        this.setState({
+            count: this.state.count+1
+        })
+        // this.setState((prevState) => {
+        //     return {
+        //         count: prevState.count + 1
+        //     };
+        // });
         // this.state.count = this.state.count +1;
         console.log(this.state);
     };
 
     handleMinusOne() {
-        this.setState((prevState) => {
-            return {
-                count: prevState.count-1
-            }
+        // this.setState((prevState) => {
+        //     return {
+        //         count: prevState.count-1
+        //     }
+        // }) 
+        this.setState({ //prevState에 직접접근
+            count: this.state.count -1
         })
         console.log(this.state);
     };
 
     handleReset() {
-        this.setState(() => {
+        // this.setState(() => {
+        //     return {
+        //         count: 0 
+        //     }
+        // })
+        this.setState(() => { // 이 방식의 setState는 component render를 두번하게된다(아래함수도 실행)
             return {
-                count: 0 
-            }
+                count: 0
+            };
+        });
+
+        this.setState((prevState) => {
+            return {
+                count: prevState.count +1
+            };
         })
+
     };
 
     render(){
