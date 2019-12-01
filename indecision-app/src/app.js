b37a8c1d94092dc09c53ae7db104195b129d1a04
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-01 15:43:50 +0900

    import / export

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,2 +1,9 @@
-console.log('this is ne wone ')
-console.log('this is new onne2 ');
+import { square, add } from './utils.js';
+import { isAdult, canDrink } from'./person';
+
+console.log(add(3,4));
+console.log(square(3));
+
+
+console.log(isAdult(19));
+console.log(canDrink(22));
--- indecision-app/src/utils.js
+++ indecision-app/src/utils.js
@@ -1,1 +1,12 @@
 console.log('this is utils.js');
+
+// const square = (x) => x*x;
+// const add = (a,b) => a+b;
+
+// export {
+//     square,
+//     add
+// }
+
+export const square = (x) => x * x;
+export const add = (a,b) => a + b;  
