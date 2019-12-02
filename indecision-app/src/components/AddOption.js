fbecee64c270ba4ae9b3253aceaeafca3e89af77
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-02 13:30:39 +0900

    Source Map with webpack

--- indecision-app/src/components/AddOption.js
+++ indecision-app/src/components/AddOption.js
@@ -11,9 +11,10 @@ export default class AddOption extends React.Component {
 
   handleAddOption(e) { // 아래에 props로 상위클래스에서 가져온 handleAddOption이 있지만 여기서도 같은 이름으로 함수를 설정가능하다
     e.preventDefault();
+
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
-    
+  
     this.setState(() => ({error}));
     if(!error){
       e.target.elements.option.value = '';
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -5,7 +5,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Delete All</button> 
-      {props.options.length === 0 && <p>Please add an Option to get started! </p>}
+      {props.options.length === 0 && <p>Please add an Option to get started!</p>}
       {props.options.map(option => (
         <Option 
           key={option} 
