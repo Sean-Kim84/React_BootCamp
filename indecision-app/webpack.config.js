7474bd45998be9b15949b58bdfd5e9243e8eff0d
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-02 18:26:14 +0900

    Setting Up webpack with SCSS

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -12,6 +12,15 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
+        }, {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ],
+            exclude: /node_modules/
+            
         }]
     },
     devtool: 'cheap-module-eval-source-map', // chrome에서 어디에 error가 발생했는지 정확하게 알려준다
