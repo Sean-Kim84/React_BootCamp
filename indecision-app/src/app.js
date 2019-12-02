a86ee1d9144278b89e8a6d68ed314248b340269f
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-02 17:33:34 +0900

    Passing children to props

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app')) 
+const Layout = (props) => {
+  return (
+    <div>
+      <p>Header</p>
+      {props.children}
+      <p>Footer</p>
+    </div>
+  )
+}
 
+
+ReactDOM.render(<Layout>
+    <div>
+      <h1>Page Title</h1>
+      <p>This is my page</p>
+    </div>
+  </Layout>, document.getElementById('app')) 
+
+
+
+
+/* babel-plugin-class-transform-properties */
 class OldSyntax {
     constructor() {
         this.name = "Seankim";
